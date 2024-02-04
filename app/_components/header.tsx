4cfc6721b273787e751d83b36d4cab3b361cbbf0
Denison Kolling <denisonkolling@gmail.com>
2024-02-04 09:54:53 -0300

    feat: add o auth provider

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,17 +5,30 @@ import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data } = useSession();
+
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-5">
         <Link href="/">
           <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
         </Link>
-        <Button variant="outline" size="icon">
-          <MenuIcon size={16} />
-        </Button>
+
+        {data?.user ? (
+          <div>
+            <span className="px-2">{data.user.name}</span>
+            <Button onClick={() => signOut()}>Logout</Button>
+          </div>
+        ) : (
+          <Button onClick={handleLoginClick}>Login</Button>
+        )}
       </CardContent>
     </Card>
   );
