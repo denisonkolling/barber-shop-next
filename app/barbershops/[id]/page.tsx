399b50e52c46684767c733014d9edbd69c34b5bc
Denison Kolling <denisonkolling@gmail.com>
2024-02-05 09:52:56 -0300

    feat: update next auth export and handle isAuthenticated

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,7 +1,8 @@
 import { db } from "@/app/_lib/prisma";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
-
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 interface BarbershopDetailsPageProps {
   params: {
@@ -9,7 +10,9 @@ interface BarbershopDetailsPageProps {
   };
 }
 
-const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
+const BarbershopDetailsPage = async ({ params, }: BarbershopDetailsPageProps) => {
+
+  const session = await getServerSession(authOptions);
 
   if (!params.id) {
     //TODO: redirecionar para home page
@@ -17,12 +20,8 @@ const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
   }
 
   const barbershop = await db.barbershop.findUnique({
-    where: {
-      id: params.id,
-    },
-    include: {
-      services: true,
-    },
+    where: { id: params.id},
+    include: {services: true},
   });
 
   if (!barbershop) {
@@ -31,17 +30,16 @@ const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
   }
 
   return (
-  
     <div>
-    <BarbershopInfo barbershop={barbershop} />
+      <BarbershopInfo barbershop={barbershop} />
 
-    <div className="px-5 flex flex-col gap-4 py-6">
-      {barbershop.services.map((service) => (
-        <ServiceItem key={service.id} service={service} />
-      ))}
+      <div className="flex flex-col gap-4 px-5 py-6">
+        {barbershop.services.map((service) => (
+          <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user}/>
+        ))}
+      </div>
     </div>
-  </div>
-  )
+  );
 };
 
 export default BarbershopDetailsPage;
